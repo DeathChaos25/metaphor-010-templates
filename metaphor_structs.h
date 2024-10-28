af17e4b25d5757336fe7de4cf2dc75fd8d22f667
DeathChaos25 <deathchaos25@gmail.com>
2024-10-28 17:49:26 -0400

    skill stuff

--- metaphor_structs.h
+++ metaphor_structs.h
@@ -1,3 +1,88 @@
+LittleEndian();
+
+typedef struct
+{
+    bool UseMenu : 1 <name = "Usable Outside of Battle">;
+    bool UseBattle : 1 <name = "Usable in Battle">;
+    bool bit3 : 1;
+    bool BackRowOnly : 1 <name = "Usable Only in Back Row">;
+    byte : 1;
+    byte : 1;
+    byte : 1;
+    byte : 1;
+} SkillUseConditions;
+
+typedef struct
+{
+    byte : 1;
+    byte : 1;
+    byte : 1;
+    byte : 1;
+    bool TargetSelf : 1 <name = "Self">;
+    bool TargetAlly : 1 <name = "Ally (Alive)">;
+    bool bit3 : 1;
+    bool TargetAllyDead : 1 <name = "Ally (Dead)">;
+} TargetCondition;
+
+enum <byte> CostTypeEnum
+{
+    HP = 1,
+    MP = 2,
+    Money = 3
+};
+
+enum <byte> TargetCount
+{
+    SingleFoe = 0,
+    AllFoes = 1
+};
+
+enum <byte> PassiveOrActive
+{
+    Active = 0,
+    Passive = 1
+};
+
+enum <byte> SkillElement
+{
+    Slash = 0,
+    Pierce = 1,
+    Strike = 2,
+    Fire,
+    Ice,
+    Electric,
+    Wind,
+    Light,
+    Dark,
+    Almighty
+};
+
+enum <byte> ApplyOrCure
+{
+    DummyApplyCure = 0,
+    Apply = 1,
+    Cure = 2
+};
+
+enum <byte> TargetSide
+{
+    DummyTarSide = 0,
+    Target_Allies = 1,
+    Target_Foes = 2
+};
+
+typedef struct
+{
+    bool Phys : 1 <name = "Physical Damage Skill">;
+    bool Mag : 1 <name = "Magical Damage Skill">;
+    byte : 1;
+    byte : 1;
+    byte : 1;
+    byte : 1;
+    byte : 1;
+    byte : 1;
+} PhysOrMagType;
+
 typedef struct
 {
     byte : 1;
